f2843b1389b59cbdf1f033e8aafd17b67f479101
Sanket <sanketlives@gmail.com>
2025-03-17 19:23:19 +0530

    module 3

--- pratice.py
+++ pratice.py
@@ -156,13 +156,33 @@
 
 
 
-import pandas as pd
+# import pandas as pd
 
-# Load NYC Taxi Dataset
-url = "https://raw.githubusercontent.com/mwaskom/seaborn-data/master/taxis.csv"
-df = pd.read_csv(url)
+# # Load NYC Taxi Dataset
+# url = "https://raw.githubusercontent.com/mwaskom/seaborn-data/master/taxis.csv"
+# df = pd.read_csv(url)
 
-print("NYC Taxi Dataset:\n", df.head())
+# print("NYC Taxi Dataset:\n", df.head())
 
 
 
+
+class Car:
+    def __init__(self, brand, model):
+        self.brand = brand  # Encapsulation
+        self.model = model
+
+    def display_info(self):  # Abstraction
+        return f"Car Brand: {self.brand}, Model: {self.model}"
+
+# Inheritance and Polymorphism
+class ElectricCar(Car):
+    def __init__(self, brand, model, battery_capacity):
+        super().__init__(brand, model)
+        self.battery_capacity = battery_capacity
+
+    def display_info(self):  # Polymorphism
+        return f"{super().display_info()}, Battery: {self.battery_capacity} kWh"
+
+car1 = ElectricCar("Tesla", "Model S", 100)
+print(car1.display_info())
