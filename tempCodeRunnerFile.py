02ac585f2ca38ae1efa8ed560aad37951d93f00f
Sanket <sanketlives@gmail.com>
2025-03-18 20:25:42 +0530

    updated

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,20 +1,20 @@
-# class Car:
-# #     def __init__(self, brand, model):
-# #         self.brand = brand  # Encapsulation
-# #         self.model = model
+class Car:
+    def __init__(self, brand, model):
+        self.brand = brand  # Encapsulation
+        self.model = model
 
-# #     def display_info(self):  # Abstraction
-# #         return f"Car Brand: {self.brand}, Model: {self.model}"
+    def display_info(self):  # Abstraction
+        return f"Car Brand: {self.brand}, Model: {self.model}"
 
-# # # Inheritance and Polymorphism
-# # class ElectricCar(Car):
-# #     def __init__(self, brand, model, battery_capacity):
-# #         super().__init__(brand, model)
-# #         self.battery_capacity = battery_capacity
+# Inheritance and Polymorphism
+class ElectricCar(Car):
+    def __init__(self, brand, model, battery_capacity):
+        super().__init__(brand, model)
+        self.battery_capacity = battery_capacity
 
-# #     def display_info(self):  # Polymorphism
-# #         return f"{super().display_info()}, Battery: {self.battery_capacity} kWh"
+    def display_info(self):  # Polymorphism
+        return f"{super().display_info()}, Battery: {self.battery_capacity} kWh"
 
-# # car1 = ElectricCar("Tesla", "Model S", 100)
-# # print(car1.display_info())
+car1 = ElectricCar("Tesla", "Model S", 100)
+print(car1.display_info())
 
